6efd8e37cb1c70b20d89ab197f870937d0f029d7
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2020-09-03 23:08:51 +0800

    Update login-auth.js

--- login-auth.js
+++ login-auth.js
@@ -10,17 +10,24 @@ loginButton.addEventListener("click", (e) => {
   const password = loginForm.password.value;
 
   if (username === "admin" && password === "password") {
+    loginErrorMsg.style.opacity = 0;
+    loginUsernameErrorField.style.borderBottom = "1px solid #4caf50"
+    loginPasswordErrorField.style.borderBottom = "1px solid #4caf50"
+    setTimeout(function () {
       alert("You have successfully logged in.");
       location.reload();
+    }, 100);
   } if (username !== "admin") {
     loginErrorMsg.style.opacity = 1;
     loginUsernameErrorField.style.borderBottom = "1px solid #fe3030"
   } if (password !== "password") {
     loginErrorMsg.style.opacity = 1;
     loginPasswordErrorField.style.borderBottom = "1px solid #fe3030"
-  } else {
+  } if (username === "admin") {
     loginErrorMsg.style.opacity = 1;
-    loginUsernameErrorField.style.borderBottom = "1px solid #fe3030"
-    loginPasswordErrorField.style.borderBottom = "1px solid #fe3030"
+    loginUsernameErrorField.style.borderBottom = "1px solid #4caf50"
+  } if (password === "password") {
+    loginErrorMsg.style.opacity = 1;
+    loginPasswordErrorField.style.borderBottom = "1px solid #4caf50"
   }
 })
