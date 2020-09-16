9e9f812c4ddd96703dd4997b864373d0120a18b6
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2020-09-16 23:39:50 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -7,7 +7,6 @@
         <div class="footer-ip-widget">
           <div id="findipwidget"></div>
           <div class="findiplink" id="findipurl">Powered by <a href="http://www.find-ip.net/" target="_blank">Find-IP.net</a></div>
-          <script defer src="//api.find-ip.net/widget.js?width=1110&"></script>
         </div>
         <hr>
       </div>
@@ -21,6 +20,9 @@ Bootstrap core JavaScript - don't touch! -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+<!-- Find-IP Widget -->
+<script defer src="//api.find-ip.net/widget.js?width=1110&"></script>
 
 </body>
 
