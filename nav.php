abb93434d92ff8476280b6f854e6ac7a5d6040e5
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2020-09-16 23:40:04 +0800

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,70 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta http-equiv="Content-Type" charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Maring Network main website." />
+  <title>Maring Network</title>
+
+  <script async src='https://www.google-analytics.com/analytics.js'></script>
+  <!-- Bootstrap core CSS -->
+  <link href="stylesheets/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="stylesheets/jumbotron.css" rel="stylesheet">
+
+  <!-- Global styles -->
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
+  <!-- Local styles -->
+  <link rel="stylesheet" type="text/css" href="login_style.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="/maring-main-html/favicon/favicon.ico" type="favicon.ico" />
+  <link rel="icon" href="/maring-main-html/favicon/favicon.ico" type="favicon.ico" />
+  <link rel="apple-touch-icon" sizes="57x57" href="/maring-main-html/favicon/apple-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="60x60" href="/maring-main-html/favicon/apple-icon-60x60.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="/maring-main-html/favicon/apple-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="/maring-main-html/favicon/apple-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="/maring-main-html/favicon/apple-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="/maring-main-html/favicon/apple-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="/maring-main-html/favicon/apple-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="/maring-main-html/favicon/apple-icon-152x152.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/maring-main-html/favicon/apple-icon-180x180.png" />
+  <link rel="icon" type="image/png" sizes="192x192" href="/maring-main-html/favicon/android-icon-192x192.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/maring-main-html/favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="96x96" href="/maring-main-html/favicon/favicon-96x96.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/maring-main-html/favicon/favicon-16x16.png" />
+  <link rel="manifest" href="/maring-main-html/favicon/manifest.json" />
+  <meta name="msapplication-TileColor" content="#ffffff" />
+  <meta name="msapplication-TileImage" content="/maring-main-html/favicon/ms-icon-144x144.png" />
+  <meta name="theme-color" content="#ffffff" />
+</head>
+
+<body>
+  <header id="main-header">
+    <div class="navigation">
+      <div class="nav-header">
+        <nav class="nav-header-container navbar-expand-sm">
+          <div class="nav-logo-container">
+            <a href="index.php">
+              <img class="nav-header-logo" src="images/mystWiz-sat.jpg" alt="logo">
+            </a>
+            <a class="nav-header-logo-text" href="index.php">
+              <img class="nav-header-logo-text" src="images/mysticalWizard-font-header.jpg" alt="Mystical Wizard">
+            </a>
+          </div>
+          <div class="nav-tools-container">
+            <div class="nav-tools-back-to-home">
+              <a class="btn btn-outline-secondary" href="index.php" role="button">Home</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+  </header> <!-- /header -->
