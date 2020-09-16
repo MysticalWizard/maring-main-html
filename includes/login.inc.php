2cc65e0d36acb5a53c5a2226608f321fc36c9222
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2020-09-16 23:39:56 +0800

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -8,14 +8,14 @@ if (isset($_POST['login-submit'])) {
   $password = $_POST['pwd'];
 
   if (empty($mailuid) || empty($password)) {
-    header("Location: ../login.php?error=emptyfields")
+    header("Location: ../login.php?error=emptyfields");
     exit();
   }
   else {
     $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-      header("Location: ../login.php?error=sqlerror")
+      header("Location: ../login.php?error=sqlerror");
       exit();
     }
     else {
@@ -23,10 +23,10 @@ if (isset($_POST['login-submit'])) {
       mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
       mysqli_stmt_execute($stmt);
       $result = mysqli_stmt_get_result($stmt);
-      if ($row = mysql_fetch_assoc($result)) {
-        $pwdCheck = password_verify($pwdCheck, $row['pwdUsers']);
+      if ($row = mysqli_fetch_assoc($result)) {
+        $pwdCheck = password_verify($password, $row['pwdUsers']);
         if ($pwdCheck == false) {
-          header("Location: ../login.php?error=incorrectpwd")
+          header("Location: ../login.php?error=incorrectpwd");
           exit();
         }
         else if ($pwdCheck == true) {
@@ -34,16 +34,16 @@ if (isset($_POST['login-submit'])) {
           $_SESSION['userId'] = $row['idUsers'];
           $_SESSION['userUid'] = $row['uidUsers'];
 
-          header("Location: ../index.php?login=success")
+          header("Location: ../index.php?login=success");
           exit();
         }
         else {
-          header("Location: ../login.php?error=invalidpwd")
+          header("Location: ../login.php?error=invalidpwd");
           exit();
         }
       }
       else {
-        header("Location: ../login.php?error=nouser")
+        header("Location: ../login.php?error=nouser");
         exit();
       }
 
