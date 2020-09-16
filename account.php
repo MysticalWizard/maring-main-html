a4dc68d01a7bf64a779effee61b5fc91bb958f1f
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2020-09-17 02:55:01 +0800

    Create account.php

---
+++ account.php
@@ -0,0 +1,24 @@
+<?php
+  require 'header.php';
+
+  require 'includes/dbh.inc.php';
+?>
+  <div id="content">
+    <div class="main">
+      <p><a href="index.php">index.php</a></p>
+      <br><br><br>
+      <main role="main">
+        <!-- Main jumbotron for a primary marketing message or call to action -->
+        <div class="jumbotron">
+          <div class="container">
+            <h1 class="display-3">My Account</h1>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <p>Coming soon!</p>
+      </div>
+    </div>
+<?php
+  require 'footer.php';
+?>
