5dee451830be0c442c7955382e162c1d8e971bbd
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2020-09-21 23:07:48 +0800

    Update html404.php

--- html404.php
+++ html404.php
@@ -1,3 +1,6 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -7,8 +10,14 @@
     <meta name="description" content="Maring Network: 404" />
     <title>404 : Page not found</title>
 
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron.css" rel="stylesheet">
+
     <!-- Global styles -->
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="/style.css" />
 
     <!-- Local styles -->
     <link rel="stylesheet" href="/css/html404.css">
@@ -37,6 +46,45 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
   </head>
   <body>
+    <header id="main-header">
+      <div class="navigation">
+        <div class="nav-header">
+          <nav class="nav-header-container navbar-expand-sm">
+            <div class="nav-logo-container">
+              <a href="/index.html">
+                <img class="nav-header-logo" src="/images/mystWiz-sat.jpg" alt="logo">
+              </a>
+              <a class="nav-header-logo-text" href="/index.html">
+                <img class="nav-header-logo-text" src="/images/mysticalWizard-font-header.jpg" alt="Mystical Wizard">
+              </a>
+            </div>
+            <div class="nav-tools-container">
+              <?php
+                if (isset($_SESSION['userId'])) {
+                ?> <!-- IF Logged In -->
+                  <a class="nav-account nav-pills justify-content-end" href="/account.php"><i class="far fa-user-circle"></i></a>
+                  <div class="nav-logout nav-pills justify-content-end">
+                    <form id="log-out" class="nav-pills" action="/includes/logout.inc.php" method="post">
+                      <button class="nav-logout-button btn btn-secondary" type="submit" name="logout-submit">Log Out</button>
+                    </form>
+                  </div>
+                <?php } else { ?> <!-- IF Logged Out -->
+
+                  <a class="nav-account nav-pills justify-content-end" href="#" data-toggle="modal" data-target="#accNotLoggedIn"><i class="far fa-user-circle"></i></a>
+
+                  <div class="nav-login nav-pills justify-content-end">
+                    <a class="btn btn-primary" href="/login.php" role="button">
+                      <p class="nav-tools-login">Log In</p>
+                    </a>
+                  </div>
+                <?php
+                }
+              ?>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
     <div id="content">
       <div class="container">
         <svg width="380px" height="500px" viewBox="0 0 837 1045" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
@@ -50,15 +98,23 @@
         </svg>
         <div class="message-box">
           <h1>404</h1>
-          <p>Page not found</p>
+          <p>Page not found!</p>
           <div class="buttons-con">
             <div class="action-link-wrap">
               <a onclick="history.back(-1)" class="link-button link-back-button">Go Back</a>
-              <a href="" class="link-button">Go to Home Page</a>
+              <a href="/index.php" class="link-button">Go to Home Page</a>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <!-- =======================================
+    Bootstrap core JavaScript - don't touch! -->
+
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
   </body>
 </html>
