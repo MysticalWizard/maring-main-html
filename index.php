a8d4436e75385b8d314f5433028bf49a6ca420f9
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2020-09-22 00:21:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,16 +16,47 @@
           <?php
             if (isset($_SESSION['userId'])) {
             ?> <!-- IF Logged In -->
-              <h1 class="display-3">Hello there, <?php echo($_SESSION['userUid']) ?>!</h1>
-              <p>You are logged in as <a href="account.php" target="_blank"><?php echo$_SESSION['userUid']?></a>, Have a nice day.</p>
-
+              <div class="d-flex flex-row justify-content-between">
+                <div class="jt-l">
+                  <h1 class="display-3">Hello there, <?php echo($_SESSION['userUid']) ?>!</h1>
+                  <p>You are logged in as <a href="account.php" target="_blank"><?php echo$_SESSION['userUid']?></a>, Have a nice day.</p>
+                </div>
+                <div class="jt-r flex-row-reverse">
+                  <svg height="240px" viewBox="0 0 837 1062" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                      <path d="M353,9 L626.664028,170 L626.664028,487 L353,642 L79.3359724,487 L79.3359724,170 L353,9 Z" id="Polygon-1" stroke="#007FB2" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                      <path d="M78.5,529 L147,569.186414 L147,648.311216 L78.5,687 L10,648.311216 L10,569.186414 L78.5,529 Z" id="Polygon-2" stroke="#EF4A5B" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                      <path d="M773,186 L827,217.538705 L827,279.636651 L773,310 L719,279.636651 L719,217.538705 L773,186 Z" id="Polygon-3" stroke="#795D9C" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                      <path d="M639,529 L773,607.846761 L773,763.091627 L639,839 L505,763.091627 L505,607.846761 L639,529 Z" id="Polygon-4" stroke="#F2773F" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                      <path d="M281,801 L383,861.025276 L383,979.21169 L281,1037 L179,979.21169 L179,861.025276 L281,801 Z" id="Polygon-5" stroke="#36B455" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                    </g>
+                  </svg>
+                </div>
+              </div>
 
             <?php } else { ?> <!-- IF Logged Out -->
-              <h1 class="display-3">Hello there!</h1>
-              <p>Please sign in to gain full access to the page. <br>Thank you, Have a nice day.</p>
-              <div class="sign-display">
-                <a class="btn btn-primary btn-lg" href="login.php" type="button">Sign In »</a>
-                <a class="btn btn-primary btn-lg" href="register.php" type="button">Sign Up »</a>
+              <div class="d-flex flex-row justify-content-between">
+                <div class="jt-l">
+                  <h1 class="display-3">Hello there!</h1>
+                  <p>Please sign in to gain full access to the page. <br>Thank you, Have a nice day.</p>
+                  <div class="sign-display">
+                    <a class="btn btn-primary btn-lg" href="login.php" type="button">Sign In »</a>
+                    <a class="btn btn-primary btn-lg" href="register.php" type="button">Sign Up »</a>
+                  </div>
+                </div>
+                <div class="jt-r flex-row-reverse">
+                  <div class="jt-svg-polygon">
+                    <svg height="240px" viewBox="0 0 837 1062" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                        <path d="M353,9 L626.664028,170 L626.664028,487 L353,642 L79.3359724,487 L79.3359724,170 L353,9 Z" id="Polygon-1" stroke="#007FB2" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                        <path d="M78.5,529 L147,569.186414 L147,648.311216 L78.5,687 L10,648.311216 L10,569.186414 L78.5,529 Z" id="Polygon-2" stroke="#EF4A5B" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                        <path d="M773,186 L827,217.538705 L827,279.636651 L773,310 L719,279.636651 L719,217.538705 L773,186 Z" id="Polygon-3" stroke="#795D9C" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                        <path d="M639,529 L773,607.846761 L773,763.091627 L639,839 L505,763.091627 L505,607.846761 L639,529 Z" id="Polygon-4" stroke="#F2773F" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                        <path d="M281,801 L383,861.025276 L383,979.21169 L281,1037 L179,979.21169 L179,861.025276 L281,801 Z" id="Polygon-5" stroke="#36B455" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                      </g>
+                    </svg>
+                  </div>
+                </div>
               </div>
             <?php
             }
